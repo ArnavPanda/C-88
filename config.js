4dd0318248a1b631c499d944e2ccbc648772862b
ArnavPanda <79236877+ArnavPanda@users.noreply.github.com>
2021-10-06 18:42:21 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyAW_8hRJqczz7dSfSXP4XW8eT637rZnzd0",
+  authDomain: "storytelling-app-e0959.firebaseapp.com",
+  projectId: "storytelling-app-e0959",
+  storageBucket: "storytelling-app-e0959.appspot.com",
+  messagingSenderId: "560501346170",
+  appId: "1:560501346170:web:34288d6e4af6031fe2d0c3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
